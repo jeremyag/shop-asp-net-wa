eff1a6b7e14b54b8aec6c3ca4b474f8dfaf333a1
Jeremy Agcaoili <jeremy.agcaoili@luxclusif.com>
2023-09-23 14:20:23 +0800

    Added Controller, Model and Views.

---
+++ shop-asp-net-wa/Controllers/PieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shop_asp_net_wa.Models;
+using shop_asp_net_wa.ViewModels;
+
+namespace shop_asp_net_wa.Controllers;
+
+public class PieController : Controller
+{
+    private readonly IPieRepository _pieRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+    {
+        _pieRepository = pieRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+    public IActionResult List()
+    {
+        PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+        return View(pieListViewModel);
+    }
+}
---
+++ shop-asp-net-wa/ViewModels/PieListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using shop_asp_net_wa.Models;
+
+namespace shop_asp_net_wa.ViewModels;
+
+public class PieListViewModel
+{
+    public IEnumerable<Pie> Pies { get; }
+    public string? CurrentCategory { get; }
+    public PieListViewModel(IEnumerable<Pie> pies, string? currentCategory)
+    {
+        Pies = pies;
+        CurrentCategory = currentCategory;
+    }
+}
